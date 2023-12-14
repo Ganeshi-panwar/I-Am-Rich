982c75e0559c57a4c469c4fef0f8d1e97f3c210d
Ganeshi <ganeshi8279@gmail.com>
2023-12-14 16:36:11 +0530

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Yatharth Rawat on 14/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
